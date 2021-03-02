1537c2afae0591982723097d7c59e2b95507804e
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-02 23:58:38 +0900

    トップページ作成のためproductモデル作成

---
+++ db/migrate/20210302143807_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string     :title,              null: false
+      t.text       :description_text,   null: false
+      t.integer    :category_id,        null: false
+      t.integer    :product_status_id,  null: false
+      t.integer    :prefectures_id,     null: false
+      t.integer    :shipment_source_id, null: false
+      t.integer    :days_id,            null: false
+      t.integer    :price,              null: false
+      t.references :user,               null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
