6d39c917732c2a3771e76ade1a6d2a873dc2e3bc
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-02 16:32:58 +0900

    パスワードバリテーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, 
+
+  validate :password_complexty
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,100}$/
+
+    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
 end
