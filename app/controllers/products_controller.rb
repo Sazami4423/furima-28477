34180e70e91b62af0e990b92802c083d6cb1de9a
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-05 12:33:06 +0900

    画像アップロードのためのストロングパラメータ追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,15 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @products = Product.new
+  end
   
+
+  private
+
+  def product_params
+    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 end
