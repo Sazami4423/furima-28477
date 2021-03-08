7dc4e77cba8a12606f5e863582a69a8a0602baad
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-08 14:52:37 +0900

    商品出品機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,29 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @product = Product.all
   end
 
   def new
-    @products = Product.new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    binding.pry
+    if @product.save
+         redirect_to root_path
+    else
+      render :new
+    end
   end
   
 
   private
 
   def product_params
-    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:product).permit(:image, :title, :description_text, :category_id, 
+      :product_status_id, :shipping_fee_id, :prefectures_id, :days_id, :price,).merge(user_id: current_user.id)
   end
 
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ApplicationRecord
   belongs_to :days
 
   #空の投稿を保存できないようにする
-  validates :title, :text, presence: true
+  validates :title, :description_text, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 } 
