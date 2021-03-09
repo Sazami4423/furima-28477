691e7df50235d58fe923e5da8def5f6a29cf604b
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-09 12:42:13 +0900

    varからletに変更

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,23 +1,23 @@
 $(function(){
-  var $price_input = $('#item-price');
-  var $price_fee = $('#add-tax-price');
-  var fee_rate = 0.1; 
-  var maxPriceInput = 99999999; 
-  var maxPriceFee = Math.floor(maxPriceInput * fee_rate); 
-  var maxProfit  = Math.floor(maxPriceInput - maxPriceFee); 
+  let $price_input = $('#item-price');
+  let $price_fee = $('#add-tax-price');
+  let fee_rate = 0.1; 
+  let maxPriceInput = 99999999; 
+  let maxPriceFee = Math.floor(maxPriceInput * fee_rate); 
+  let maxProfit  = Math.floor(maxPriceInput - maxPriceFee); 
   $('#price-box').attr('autocomplete', 'off');
   $('#fee-box').text('-')
   $('#profits-box').text('-')
   $price_input.on('input', function(event) {
-    var input_val = $(this).val();  
-    var input_val_num = Number(input_val.replace(/[^[0-9]+$/g, '')); 
+    let input_val = $(this).val();  
+    let input_val_num = Number(input_val.replace(/[^[0-9]+$/g, '')); 
     if(input_val_num == 0) {  
     } else if (input_val_num > maxPriceInput) { 
       input_val_num = maxPriceInput;
     }
     $(this).val(input_val_num);  
     if(input_val_num != 0) { 
-      var total_price_fee = Math.floor(input_val_num * fee_rate); 
+       total_price_fee = Math.floor(input_val_num * fee_rate); 
     }  else if (total_price_fee > maxPriceFee) {
       total_price_fee = maxPriceFee;
     }
@@ -28,9 +28,9 @@ $(function(){
     else if(total_price_fee  == undefined){  
       $price_fee.text('-');
     }
-    var $price_profit = $('#profit');  
+    let $price_profit = $('#profit');  
     if(input_val_num != 0) { 
-      var total_profit = Math.floor(input_val_num - total_price_fee); 
+       total_profit = Math.floor(input_val_num - total_price_fee); 
     }  else if (total_profit > maxProfit) { 
      total_profit = maxProfit;
     }
