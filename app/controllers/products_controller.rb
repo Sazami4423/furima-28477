72b046acec7cdf58c5bedc50a88196a612e39928
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-10 19:12:31 +0900

    違うユーザーが商品の編集ができないように条件を追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update]
+  before_action :user_check, only: [:edit, :update]
 
   def index
     @products = Product.includes(:user).order('created_at DESC')
@@ -42,5 +43,11 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def user_check
+    if @product.user.id != current_user.id 
+      redirect_to root_path 
+    end  
   end
 end
