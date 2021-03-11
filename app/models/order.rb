f6d93692041fca6bd54d3ae7402278e7e9afdf87
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-11 16:06:23 +0900

    バリテーションを追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,17 @@
 class Order < ApplicationRecord
+
+  # tokenの値を取り扱えるようにする
+  attr_accessor :token
+
+  # アソシエーション
   belongs_to :product_purchase_management
+
+  # バリテーション
+  validates :prefectures_id, numericality: { other_than: 1 }
+  with_options  presence: true do
+    validates :postal_code
+    validates :municipalities 
+    validates :address
+    validates :tel
+  end
 end
