48c565a02394573c78f0f5d58180ae7b6ed32b64
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-12 22:13:54 +0900

    決済処理の若干の修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -9,16 +9,16 @@ const pay = () => {
 
     const card = {
       number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
       exp_month: formData.get("order[exp_month]"),
       exp_year: `20${formData.get("order[exp_year]")}`,
-      cvc: formData.get("order[cvc]"),
     };
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        const tokenObj = `<input value=${token} name='token' type="hidden" > `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
       // フォーム情報を削除
