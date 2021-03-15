c4ff5806d3de631c1947f2d3ce29ca11a3538f53
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-15 18:24:58 +0900

    S3の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
+  # 画像保存先の指定
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
