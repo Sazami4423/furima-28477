fb5c16948a44aad1721a99ae971372fcc11c417c
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-03-15 15:45:42 +0900

    saveメソッドの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,8 +17,7 @@ class Order
   validates :tel, format: { with: /\A\d{10,11}\z/ }
 
   def save
-    ProductPurchaseManagement.create(user_id: user_id, product_id: product_id)
-    product_purchase_management = ProductPurchaseManagement.find_by(product_id: product_id)
+    product_purchase_management = ProductPurchaseManagement.create(user_id: user_id, product_id: product_id) 
     Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipalities: municipalities, address: address,
                    building_name: building_name, tel: tel, product_purchase_management_id: product_purchase_management.id)
   end
